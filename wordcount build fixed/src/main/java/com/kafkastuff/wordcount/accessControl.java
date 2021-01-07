dd646cc8a5827e1b5ea537f2df78c984de84dc0e
Dcoder99 <deeps.karanji2@gmail.com>
2021-01-07 18:11:36 +0530

    Created template for mapping

---
+++ wordcount build fixed/src/main/java/com/kafkastuff/wordcount/accessControl.java
@@ -0,0 +1,20 @@
+package com.kafkastuff.wordcount;
+import java.util.Properties;
+import java.util.Collections;
+import java.util.Set;
+import org.apache.kafka.clients.producer.Producer;
+//import com.kafkastuff.wordcount.KafkaProducer;
+//import com.kafkastuff.wordcount.Producer_kafka;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.common.securekafkastuff.Topics;
+
+public class accessControl {
+  // class for CRUD of mapping we have come up with
+}
